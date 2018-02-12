cdb699cd847b04c128d86ef87dcd6a195b78d44c
Victor Yap <victor@vicyap.com>
2018-02-11 23:49:55 -0600

    Added user logins and registration

--- bkmrk/models.py
+++ bkmrk/models.py
@@ -1,16 +1,32 @@
 from datetime import datetime
-from bkmrk import db
+
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from bkmrk import db, login
 
 
-class User(db.Model):
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     book_quotes = db.relationship('BookQuote', backref='user', lazy='dynamic')
+    books = db.relationship('UserBook', backref='user', lazy='dynamic')
 
     def __repr__(self):
         return f'<User {self.username}>'
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 
 class Book(db.Model):
@@ -19,6 +35,13 @@ class Book(db.Model):
 
     def __repr__(self):
         return f'<Post {self.isbn13}>'
+
+
+def UserBook(db.Model):
+    '''Relational table for Users and Book.'''
+    id = db.Column(db.Integer, primary_key=True)
+    id_user = db.Column(db.Integer, db.ForeignKey('user.id'))
+    id_book = db.Column(db.Integer, db.ForeignKey('book.id'))
 
 
 class BookQuote(db.Model):
--- bkmrk/routes.py
+++ bkmrk/routes.py
@@ -1,22 +1,55 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
+from flask_login import current_user, login_user, logout_user, login_required
+from werkzeug.urls import url_parse
 
 from bkmrk import app
-from bkmrk.forms import LoginForm
+from bkmrk.forms import LoginForm, RegistrationForm
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    user = {'username': 'vicyap'}
-    return render_template('index.html', title='Home', user=user)
+    books = [
+        {'author': 'author', 'title': 'title'},
+    ]
+    return render_template('index.html', title='Home', books=books)
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect(url_for('index'))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(nexst_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     else:
         return render_template('login.html', title='Sign In', form=form)
 
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@app.route('/register')
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
