b3c775a4c0449cc0b31f1f740d81fd3eaa694f11
Victor Yap <victor@vicyap.com>
2018-02-14 16:00:56 -0600

    Add user profiles

--- bkmrk/models.py
+++ bkmrk/models.py
@@ -16,6 +16,9 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+
+    about_me = db.Column(db.String(140))
+
     book_quotes = db.relationship('BookQuote', backref='user', lazy='dynamic')
     books = db.relationship('UserBook', backref='user', lazy='dynamic')
 
