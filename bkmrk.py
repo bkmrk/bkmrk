8877d3c3b069c1f5c3adf26b99161e84c7137ed0
Victor Yap <victor@vicyap.com>
2018-02-24 19:19:20 -0600

    Update bkmrk.conf config, remove extra code from bkmrk.py

--- bkmrk.py
+++ bkmrk.py
@@ -1,12 +1,1 @@
-from bkmrk import app, db
-from bkmrk.models import User, Book, BookQuote
-
-
-@app.shell_context_processor
-def make_shell_context():
-    return {
-        'db': db,
-        'User': User,
-        'Book': Book,
-        'BookQuote': BookQuote,
-    }
+from bkmrk import app
