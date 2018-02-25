d363d948dca2b0476f86d9eff5a022693c71207f
Victor Yap <victor@vicyap.com>
2018-02-25 16:36:08 -0600

    import flask-moment and add to base template for later use

--- bkmrk/__init__.py
+++ bkmrk/__init__.py
@@ -4,21 +4,24 @@ import os
 
 from flask import Flask
 from flask_bootstrap import Bootstrap
-from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 from flask_mail import Mail
 from flask_migrate import Migrate
-from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+from flask_moment import Moment
 
 from .config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+
+bootstrap = Bootstrap(app)
 login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
-bootstrap = Bootstrap(app)
+migrate = Migrate(app, db)
+moment = Moment(app)
 
 if not app.debug:
     if not os.path.exists('logs'):
