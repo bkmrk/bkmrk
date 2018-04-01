305d51cc6c73640b71028048a18d9a828f20b225
Victor Yap <victor@vicyap.com>
2018-04-01 08:34:45 -0500

    Update ansible habitat role to also load services

--- ansible/roles/habitat/molecule/default/tests/test_default.py
+++ ansible/roles/habitat/molecule/default/tests/test_default.py
@@ -1,3 +1,4 @@
+import json
 import os
 
 import testinfra.utils.ansible_runner
@@ -6,9 +7,19 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_processes(host):
+    assert host.process.get(user='root', comm='hab-sup')
+    assert host.process.get(user='root', comm='hab-launch')
 
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+
+def test_habitat_cmd(host):
+    assert host.exists('hab')
+
+
+def test_habitat_service(host):
+    services_json = host.check_output('curl localhost:9631/services')
+    services = json.loads(services_json)
+    assert len(services) == 1
+    nginx_service = services[0]
+    assert nginx_service.get('pkg').get('origin') == 'core'
+    assert nginx_service.get('pkg').get('name') == 'nginx'
